8681d1b8539f09317e6aa859b093d02bd46815c9
mark odhiambo okoth <markokoth96@gmail.com>
2020-05-19 21:00:40 +0300

    responsiveness and bringing in vue

--- js/main.js
+++ js/main.js
@@ -2,7 +2,14 @@ new Vue({
     el: '#app',
     data:{
         title: 'HOME',
-        html: 90,
+        html: 95,
+        css: 90,
+        javascript:80,
+        python: 70,
+        react: 60,
+        vue:70,
+        django:65,
+        node:70,
         attached: false
     },
     methods: {
